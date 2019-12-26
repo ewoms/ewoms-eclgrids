4ebbca80603faa9e492359a639360e36f77b5b70
Andreas Lauser <and@poware.org>
2019-12-26 18:10:25 +0100

    use the trilinos supplied FindZoltan cmake module
    while it may not be perfect, at least it is the way to detect zoltan
    that is officially supported by the trilinos project.
    
    In order to make this work, the target for the "zoltan" test needed to
    be renamed to avoid a clash of target names with the official
    FindZoltan module.

--- cmake/modules/EwomsEclgridsMacros.cmake
+++ cmake/modules/EwomsEclgridsMacros.cmake
@@ -4,16 +4,11 @@
 # suggests ewoms-eclgrids!
 #
 
-# make sure that we get our FindZOLTAN.cmake module, a random dune
-# module (the version of dune-alugrid seems to be buggy when it comes
-# to our purposes)
-list(INSERT 0 CMAKE_MODULE_PATH "${ewoms-eclgrids_MODULE_PATH}")
-
-find_package(ZOLTAN)
-if(ZOLTAN_FOUND)
+find_package(Zoltan)
+if(Zoltan_FOUND)
   dune_register_package_flags(
-    LIBRARIES "${ZOLTAN_LIBRARIES}"
-    INCLUDE_DIRS "${ZOLTAN_INCLUDE_DIRS}")
+    LIBRARIES "${Zoltan_LIBRARIES}"
+    INCLUDE_DIRS "${Zoltan_INCLUDE_DIRS}")
 endif()
 
 find_package(Boost
